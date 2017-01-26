52c3a3cfdccf369dbc5f706364886ee09318a2cf
paeikela <paeikela@stud.ntnu.no>
2017-01-26 10:30:35 -0800

    Lab3 finally finished

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -16,23 +16,23 @@ function initializePage() {
 	});
 
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	 $(".project").click(projectClick);
 }
+
 function projectClick(e) { 
+  console.log("clicked")
     // prevent the page from reloading      
     e.preventDefault();
     // In an event handler, $(this) refers to      
     // the object that triggered the event      
     $(this).css("background-color", "#7fff00");
-	$(this).toggle();
-
+	  $(this).toggle();
     var containingProject = $(this).closest(".project");
     var description = $(containingProject).find(".project-description");
     if (description.length == 0) {
-       $(containingProject).append("<div class='project-description'>
-       	<p>Description of the project.</p></div>");
+       $(containingProject).append("<div class='project-description'> <p>Description of the project.</p></div>");
     } else {
        description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
-    }div>");
+    }
 
 }
